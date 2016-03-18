759e4b9715792f71c18b262c964e61d35b224ae7
LieroLarryfier <steindlmatthias@gmx.at>
2016-03-19 00:24:54 +0100

    removed bin; removed cache; improved tests; improved board;

--- app/models/Board.scala
+++ app/models/Board.scala
@@ -5,22 +5,55 @@ class Board(size: Int) {
   val playfield = Array.ofDim[Char](size,size)
   
   def placeStone(c: Char, x:Int, y:Int) {
-    if ( playfield(x)(y) == 'w' | playfield(x)(y) == 'b' ) {
-      println("ER: " + c + " not placed on " + x + "," + y)
-    } else {
-      println("OK: " + c + " placed on " + x + "," + y)
+    if ( playfield(x)(y) == 0 ) {
       playfield(x)(y) = c
+      println("OK: " + c + " placed on " + x + "," + y)
+    } else {
+       println("ER: " + c + " not placed on " + x + "," + y + " (not empty)")
     }
   }
   
   def placeManyStones(c: Char, positions: List[Point]) {
     for( pos <- positions ) {
-      placeStone(c, pos.x, pos.y)
+      playfield(pos.x)(pos.y) = c
     }
   }
   
-  def getColor(x: Int, y: Int) = {
+  def getColor(x: Int, y: Int) : Char = {
     playfield(x)(y)
+  }
+  
+  def getNeighbours(point: Point) : Array[Point] = {
+    val neighbours = new Array[Point](4)
+    val px = point.x;
+    val py = point.y;
+    
+    if ((px - 1) < 0) {
+      neighbours(0) = null
+    } else {
+      neighbours(0) = new Point(px-1,py)
+    }
+    
+    if ((px + 1) > playfield(0).length) {
+      neighbours(1) = null
+    } else {
+      neighbours(1) = new Point(px+1,py)
+    }
+    
+    if ((py - 1) < 0) {
+      neighbours(2) = null
+    } else {
+      neighbours(2) = new Point(px,py-1)
+    }
+    
+    if ((py + 1) > playfield(0).length) {
+      neighbours(3) = null
+    } else {
+      neighbours(3) = new Point(px,py+1)
+    }
+    
+    neighbours.filter(_ != null)
+    
   }
   
 }
--- test/BadukSpec.scala
+++ test/BadukSpec.scala
@@ -8,7 +8,7 @@ class BadukSpec extends PlaySpec {
   
   "After placing a stone, the board" must {
     "contain the stone" in {
-      val board = new Board(5)
+      val board = new Board(6)
       board.placeStone('w', 3, 3)
       board.getColor(3, 3) mustBe ('w')
     }    
@@ -16,7 +16,7 @@ class BadukSpec extends PlaySpec {
   
   "Placing a stone over another stone, board" must {
     "contain only the other stone" in {
-      val board = new Board(5)
+      val board = new Board(6)
       board.placeStone('w', 3, 3)
       board.placeStone('b', 3, 3)
       board.getColor(3, 3) mustBe ('w')
@@ -25,16 +25,16 @@ class BadukSpec extends PlaySpec {
   
   "When placing a next to another stone, board" must {
     "contains the other stone" in {
-      val board = new Board(5)
+      val board = new Board(6)
       board.placeStone('w', 3, 3)
       board.placeStone('b', 3, 4)
       board.getColor(3, 4) mustBe ('b')
     }    
   }
   
-  "When placing many stones, board" must {
-    "contains the stones" in {
-      val board = new Board(5)
+  "When placing many stones on board, board" must {
+    "contain the stones" in {
+      val board = new Board(6)
       board.placeManyStones('w', List(new Point(2,3), new Point(3,2), new Point(4,3), new Point(3,4)))
       board.getColor(2, 3) mustBe ('w')
       board.getColor(3, 2) mustBe ('w')
@@ -45,7 +45,7 @@ class BadukSpec extends PlaySpec {
   
   "When placing a stone to no liberties, board" must {
     "contains only the other stones" in {
-      val board = new Board(5)
+      val board = new Board(6)
       board.placeManyStones('w', List(new Point(2,3), new Point(3,2), new Point(4,3), new Point(3,4)))
       board.placeStone('b', 3, 3)
       board.getColor(2, 3) mustBe ('w')
@@ -54,5 +54,26 @@ class BadukSpec extends PlaySpec {
       board.getColor(3, 4) mustBe ('w')
       board.getColor(3, 3) must not be ('b')
     }    
+  }
+  
+  "A stone in the middle of the board" must {
+    "have 4 neighours" in {
+      val board = new Board(5)
+      board.getNeighbours(new Point(2,2)).length mustBe 4
+    }
+  }
+  
+  "A stone on the top edge of the board" must {
+    "have 3 neighours" in {
+      val board = new Board(5)
+      board.getNeighbours(new Point(0,2)).length mustBe 3
+    }
+  }
+  
+  "A stone in the top left corner of the board" must {
+    "have 2 neighours" in {
+      val board = new Board(5)
+      board.getNeighbours(new Point(0,0)).length mustBe 2
+    }
   }
 }
